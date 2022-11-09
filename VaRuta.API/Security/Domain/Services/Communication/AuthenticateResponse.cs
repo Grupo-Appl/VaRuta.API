c44fb563d6f326f171041150f7942f849eedc5f8
ClauRozasG <u201922910@upc.edu.pe>
2022-11-09 12:39:17 -0500

    add first progrees bounded context security

---
+++ VaRuta.API/Security/Domain/Services/Communication/AuthenticateResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace VaRuta.API.Security.Domain.Services.Communication;
+
+public class AuthenticateResponse
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Username { get; set; }
+    public string Token { get; set; }
+}
---
+++ VaRuta.API/Security/Domain/Services/IUserService.cs
@@ -0,0 +1,6 @@
+﻿namespace VaRuta.API.Security.Domain.Services;
+
+public interface IUserService
+{
+    
+}
