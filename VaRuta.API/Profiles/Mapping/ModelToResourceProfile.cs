cb1799a6026b73cd557130b4f21be6e59336f5ca
U201726085 <u201726085@upc.edu.pe>
2022-11-10 15:32:47 -0500

    feat(enterprise): added resource types for enterprise;

---
+++ VaRuta.API/Profiles/Mapping/ModelToResourceProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using VaRuta.API.Profiles.Domain.Models;
+using VaRuta.API.Profiles.Resources;
+
+namespace VaRuta.API.Profiles.Mapping;
+
+public class ModelToResourceProfile
+{
+    public ModelToResourceProfile()
+    {
+        CreateMap< Enterprise , EnterpriseResource>();
+    }
+}
