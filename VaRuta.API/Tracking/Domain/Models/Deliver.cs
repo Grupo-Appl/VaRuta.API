112f54e74acc3ed10f61ee4235fa0c63d4dc53ac
U201726085 <u201726085@upc.edu.pe>
2022-11-16 12:09:08 -0500

    feat(Deliver):added deliver entity model

--- VaRuta.API/Tracking/Domain/Models/Deliver.cs
+++ VaRuta.API/Tracking/Domain/Models/Deliver.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using VaRuta.API.Routing.Domain.Models;
+
+namespace VaRuta.API.Tracking.Domain.Models;
+
+public class Deliver
+{
+    public int Id { get; set;}
+    public string  Date { get; set; }
+    public string Description { get; set; }
+    public int ShipmentId { get; set; }
+    
+    public Shipment Shipment { get; set; }
+    
+}
