66fb0723fd2562e155d70966f414e33f93af600b
U201726085 <u201726085@upc.edu.pe>
2022-11-16 12:36:22 -0500

    feat(Delivery): implemented object mapping configuration.

---
+++ VaRuta.API/Tracking/Mapping/ModelToResourceProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using VaRuta.API.Tracking.Domain.Models;
+using VaRuta.API.Tracking.Resources;
+
+namespace VaRuta.API.Tracking.Mapping;
+
+public class ModelToResourceProfile : Profile
+{
+    public ModelToResourceProfile()
+    {
+        CreateMap< Delivery , DeliveryResource>();
+    }
+}
