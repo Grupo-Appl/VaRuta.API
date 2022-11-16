ac9c76c79c1a000bb5716279220719b70ba7e519
ClauRozasG <u201922910@upc.edu.pe>
2022-11-16 12:37:50 -0500

    add controller TypeOfComplaint

--- VaRuta.API/Booking/Mapping/ModelToResourceProfile.cs
+++ VaRuta.API/Booking/Mapping/ModelToResourceProfile.cs
@@ -1,6 +1,9 @@
 using AutoMapper;
 using VaRuta.API.Booking.Domain.Models;
 using VaRuta.API.Booking.Resources;
+using VaRuta.API.Publishing.Domain.Models;
+using VaRuta.API.Publishing.Resources;
+
 namespace VaRuta.API.Booking.Mapping;
 public class ModelToResourceProfile :Profile
 {
@@ -12,6 +15,7 @@ public class ModelToResourceProfile :Profile
         CreateMap< TypeOfPackage , TypeOfPackageResource>();
         CreateMap<Destination, DestinationResource>();
         CreateMap<Consignees, ConsigneesResource>();
+        CreateMap<TypeOfComplaint, TypeOfComplaintResource>();
     }
 }
 
---
+++ VaRuta.API/Publishing/Interfaces/Rest/Controllers/TypeOfComplaintController.cs
@@ -0,0 +1,73 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using VaRuta.API.Publishing.Domain.Models;
+using VaRuta.API.Publishing.Domain.Services;
+using VaRuta.API.Publishing.Resources;
+using VaRuta.API.Shared.Extensions;
+
+namespace VaRuta.API.Publishing.Interfaces.Rest.Controllers;
+
+[ApiController]
+[Route("/api/v1/[controller]")]
+public class TypeOfComplaintController: ControllerBase
+{
+     private readonly ITypeOfComplaintService _typeOfComplaintService;
+    private readonly IMapper _mapper;
+
+    public TypeOfComplaintController(ITypeOfComplaintService typeOfComplaintService, IMapper mapper)
+    {
+          _mapper = mapper;
+          _typeOfComplaintService = typeOfComplaintService;
+    }
+
+
+    [HttpGet]
+      public async Task<IEnumerable<TypeOfComplaintResource>> GetAllAsync()
+      {
+            var typeOfComplaint = await _typeOfComplaintService.ListAsync();
+            var resources = _mapper.Map<IEnumerable<TypeOfComplaint>, IEnumerable<TypeOfComplaintResource>>(typeOfComplaint);
+            return resources;
+      }
+
+      [HttpPost]
+      public async Task<IActionResult> PostAsync([FromBody] SaveTypeOfComplaintResource resource)
+      {
+            if (!ModelState.IsValid)
+                  return BadRequest(ModelState.GetErrorMessages());
+        
+            var typeOfComplaint = _mapper.Map<SaveTypeOfComplaintResource, TypeOfComplaint>(resource);
+            var result = await _typeOfComplaintService.SaveAsync(typeOfComplaint);
+            if (!result.Success)
+                  return BadRequest(result.Message);
+        
+            var typeOfComplaintResource = _mapper.Map<TypeOfComplaint, TypeOfComplaintResource>(result.Resource);
+            return Created(nameof(PostAsync), typeOfComplaintResource);
+
+      }
+
+      [HttpPut("{id}")]
+      public async Task<IActionResult> PutAsync(int id, [FromBody] SaveTypeOfComplaintResource resource)
+      {
+            if (!ModelState.IsValid)
+                  return BadRequest(ModelState.GetErrorMessages());
+            
+            var typeOfComplaint = _mapper.Map<SaveTypeOfComplaintResource, TypeOfComplaint>(resource);
+            var result = await _typeOfComplaintService.UpdateAsync(id,typeOfComplaint);
+            if (!result.Success)
+                  return BadRequest(result.Message);
+            
+            var typeOfComplaintResource = _mapper.Map<TypeOfComplaint, TypeOfComplaintResource>(result.Resource);
+            return Ok(typeOfComplaintResource);
+      }
+
+      [HttpDelete("{id}")]
+      public async Task<IActionResult> DeleteAsync(int id)
+      {
+            var result = await _typeOfComplaintService.DeleteAsync(id);
+            if (!result.Success)
+                  return BadRequest(result.Message);
+            var typeOfComplaintResource = _mapper.Map<TypeOfComplaint, TypeOfComplaintResource>(result.Resource);
+            return Ok(typeOfComplaintResource);
+
+      }
+}
