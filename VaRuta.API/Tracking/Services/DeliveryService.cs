cb471d1525cfba1836b9547b03a3e9ef786590d3
U201726085 <u201726085@upc.edu.pe>
2022-11-16 12:50:16 -0500

    feat(Delivery): added service layer implementation.

---
+++ VaRuta.API/Tracking/Services/DeliveryService.cs
@@ -0,0 +1,29 @@
+﻿using VaRuta.API.Tracking.Domain.Models;
+using VaRuta.API.Tracking.Domain.Services;
+using VaRuta.API.Tracking.Domain.Services.Communication;
+
+namespace VaRuta.API.Tracking.Services;
+
+public class DeliveryService : IDeliveryService
+{
+    private IDeliveryService _deliveryServiceImplementation;
+    public Task<IEnumerable<Delivery>> ListAsync()
+    {
+        return _deliveryServiceImplementation.ListAsync();
+    }
+
+    public Task<DeliveryResponse> SaveAsync(Delivery delivery)
+    {
+        return _deliveryServiceImplementation.SaveAsync(delivery);
+    }
+
+    public Task<DeliveryResponse> UpdateAsync(int id, Delivery delivery)
+    {
+        return _deliveryServiceImplementation.UpdateAsync(id, delivery);
+    }
+
+    public Task<DeliveryResponse> DeleteAsync(int id)
+    {
+        return _deliveryServiceImplementation.DeleteAsync(id);
+    }
+}
