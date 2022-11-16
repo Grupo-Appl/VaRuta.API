3d0bc6d1fc85038341f97ff733c4b09b296db0a9
ClauRozasG <u201922910@upc.edu.pe>
2022-11-16 12:23:46 -0500

    add service and repository TypeOfComplaint

---
+++ VaRuta.API/Publishing/Domain/Services/Communication/TypeOfComplaintResponse.cs
@@ -0,0 +1,15 @@
+﻿using VaRuta.API.Publishing.Domain.Models;
+using VaRuta.API.Shared.Domain.Services.Communication;
+
+namespace VaRuta.API.Publishing.Domain.Services.Communication;
+
+public class TypeOfComplaintResponse: BaseResponse<TypeOfComplaint>
+{
+    public TypeOfComplaintResponse(string message) : base(message)
+    {
+    }
+
+    public TypeOfComplaintResponse(TypeOfComplaint resource) : base(resource)
+    {
+    }
+}
