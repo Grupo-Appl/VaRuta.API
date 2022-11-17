6e5188b02072e084169c4dc4796eac601acfc781
ClauRozasG <u201922910@upc.edu.pe>
2022-11-17 16:13:43 -0500

    feat: add repository and profile

---
+++ VaRuta.API/Publishing/Mapping/ModelToResourceProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using VaRuta.API.Publishing.Domain.Models;
+using VaRuta.API.Publishing.Resources;
+
+
+namespace VaRuta.API.Publishing.Mapping;
+
+public class ModelToResourceProfile: Profile
+{
+    public ModelToResourceProfile()
+    {
+        CreateMap<TypeOfComplaint, TypeOfComplaintResource>();
+    }
+}
